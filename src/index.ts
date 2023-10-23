f3eec1fef1a563d105911196ff7cc4191fe67b40
wdyggh <wdyggh@gmail.com>
2023-10-23 23:13:34 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ import TMobile from "./providerModels/TMobile";
 
 checkENVs();
 const DEBUG = process.env.DEBUG === 'true' || process.env.DEBUG === '1';
+let MBsLeftOld = 10000;
 
 const run = async () => {
   const tmobile = new TMobile();
-  const MBsLeft = await tmobile.getMBsLeft();
+  const MBsLeftNew = await tmobile.getMBsLeft();
+
   if(DEBUG){
     let bundles = await tmobile.getAvaibleRoamingBundles();
     console.log(bundles);
   }
-  console.log(`${MBsLeft} MB's left`);
-  if (MBsLeft < 8000) {
+  console.log(`Old = ${MBsLeftOld} New = ${MBsLeftNew} MB's left`);
+  if ((MBsLeftNew < 2000)||((MBsLeftOld-MBsLeftNew)>2000) ){
     tmobile.requestBundle();
+    MBsLeftOld = MBsLeftNew+2000;
   }
+  
 };
 run()
 setInterval(() => {
